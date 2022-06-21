93aa5a183633fe61eac629f2295260de510f27e2
Suniel <sunieljolly@googlemail.com>
2022-06-21 14:46:02 +0700

    Title page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+        Get fit or give up and drink!
+        </h1>
+       <p>Just a group of mates giving each other shit due to how slow they all run ;)</p>
+       <p>... Coming soon</p>
       </header>
     </div>
   );
